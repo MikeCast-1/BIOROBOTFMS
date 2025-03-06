e40fd7df592fef246007e8ac2f643e83e8f4e832
MikeCast-1 <mikecast580@gmail.com>
2025-03-06 11:31:23 -0600

    Added camera test codes1.1

--- CAMERA.py
+++ CAMERA.py
@@ -1,10 +1,14 @@
-from picamera2 import Picamera2, Preview 
 import time
- 
-picam2 = Picamera2() 
-camera_config = picam2.create_preview_configuration() 
-picam2.configure(camera_config) 
-picam2.start_preview(Preview.QTGL) 
-picam2.start() 
-time.sleep(2) 
-picam2.capture_file("test.jpg")
+
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+
+preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.configure(preview_config)
+
+picam2.start()
+time.sleep(2)
+
+picam2.capture_file("test.png")
