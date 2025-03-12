3aee081ad521222d1229c2760e1707faf4929c90
MikeCast-1 <mikecast580@gmail.com>
2025-03-12 13:24:23 -0600

    Added camera driver

--- CAMERA.py
+++ CAMERA.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
+#https://github.com/raspberrypi/picamera2/blob/main/examples/capture_jpeg.py
 
 # Capture a full resolution image to memory rather than to a file.
 
+import os
 import time
 
-from picamera2 import Picamera2, Preview
+command_0 = "rpicam-still -o /home/biorobot/Desktop/test"
+img_indx = 0
+command_1 = ".jpg"
 
-picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-preview_config = picam2.create_preview_configuration()
-capture_config = picam2.create_still_configuration()
+os_command = f"{command_0}{img_indx}{command_1}"
 
-picam2.configure(preview_config)
-picam2.start()
-time.sleep(2)
-
-image = picam2.switch_mode_and_capture_image(capture_config)
-image.show()
-
-
-time.sleep(5)
-
-picam2.close()
+for i in range(5):
+    img_indx = i
+    os_command = f"{command_0}{img_indx}{command_1}"
+    XD = os.system(os_command)
+    print(XD)
